2c9c7158ad45d46af90c10a6e32d20044ea46e05
KimHyunLo <leeas016@gmail.com>
2023-01-12 14:06:48 +0900

    level 4

---
+++ level4/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:level4/authentication/login.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
---
+++ level4/lib/user/user_pref.dart
@@ -0,0 +1,11 @@
+import 'dart:convert';
+import 'package:level4/model/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class RememberUser {
+  static Future<void> saveRememberUserInfo(User userInfo) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    String userJsonData = jsonEncode(userInfo.toJson());
+    await preferences.setString("currentUser", userJsonData);
+  }
+}
